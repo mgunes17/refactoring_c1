fae8c4f33b5cd791d4c29911cbfd1040b459b3da
mgunes17 <mustafa.gunes1702@gmail.com>
2017-11-18 17:54:25 +0300

    added new method

--- src/Customer.java
+++ src/Customer.java
@@ -36,6 +36,26 @@ public class Customer {
         return result;
     }
 
+    public String htmlStatement() {
+        Enumeration _rentals = rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+
+        while (_rentals.hasMoreElements()) {
+            Rental each = (Rental) _rentals.nextElement();
+
+            //show figures for each rental
+            result += each.getMovie().getTitle()+ ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+
+        //add footer lines
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+
+        result += "On this rental you earned <EM>" + String.valueOf(
+                    getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
+        
+        return result;
+    }
+
     private double getTotalCharge() {
         double result = 0;
 
